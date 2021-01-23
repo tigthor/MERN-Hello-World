1142e4704c0825e63632edc9ea495fa57ab939b6
tigthor <tigthor1337@gmail.com>
2021-01-23 21:29:32 +0200

    adding server

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import devBundle from './devBundle'
+import path from "path";
+import template from "./../template";
+import { MongoClient } from "mongodb";
+
+const CURRENT_WORKING_DIR = process.cwd();
+
+const app = express();
+devBundle.compile(app);
+
+app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
+
+app.get("/", (req, res) => {
+  res.status(200).send(template());
+});
+
+const url = process.env.MONGODB_URI ||
+'mongodb://localhost:27017/mernSimpleSetup'
+MongoClient.connect(url, (err, db) => {
+  console.log("Connected successfully to mongodb server");
+  db.close();
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info("Server started on port %s.", port);
+});
