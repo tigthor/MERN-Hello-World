062334fd8579d4253c68f188fe3fb2fc9edda115
tigthor <tigthor1337@gmail.com>
2021-01-23 21:30:09 +0200

    adding frontend

---
+++ client/HelloWorld.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <h2>My first road</h2>
+    </div>
+  );
+};
+export default hot(module)(HelloWorld);
